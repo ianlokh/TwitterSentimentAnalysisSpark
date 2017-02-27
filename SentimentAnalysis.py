bdc632033d11f925d226f9102b9f9e08a1dff8cd
ianlokh <ian_ling@mac.com>
2017-02-27 14:27:44 +0800

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,4 +1,26 @@
-"""SimpleApp.py"""
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Code to score tweets using AFINN and to generate a set of sentiment score for each person mentioned.
+#   usage: ./bin/pyspark SentimentAnalysis.py 
+#
+
+"""SentimentAnalysis.py"""
+
 import math
 import re
 import sys
@@ -17,7 +39,7 @@ sc = SparkContext(appName="PythonSentimentAnalysis")
 sqlCtx = HiveContext(sc)
 
 # Read in the word-sentiment list and create a static RDD from it
-filenameAFINN = "/home/training/Assignment2/AFINN/AFINN-111.txt"
+filenameAFINN = "/home/training/TwitterSentimentAnalysis/AFINN/AFINN-111.txt"
 
 # map applies the lambda function (create a tuple of word and sentiment score) to every item of iterable
 # within [ ] and returns a list of results. The dictionary is used here to be able to quickly lookup the
@@ -26,7 +48,7 @@ afinn = dict(map(lambda (w, s): (w, int(s)), [ ws.strip().split('\t') for ws in 
 
 
 # Read in the candidate mapping list and create a static dictionary from it
-filenameCandidate = "file:///home/training/Assignment2/Candidates/Candidate Mapping.txt"
+filenameCandidate = "file:///home/training/TwitterSentimentAnalysis/Candidates/Candidate Mapping.txt"
 
 # map applies the lambda function
 candidates = sc.textFile(filenameCandidate).map(lambda x: (x.strip().split(",")[0],x.strip().split(","))) \
@@ -41,8 +63,9 @@ pattern_split = re.compile(r"\W+")
 
 #tweets = sqlCtx.sql("select id, text, entities.user_mentions.name, entities.hashtags.text hashtag_text from twitteranalytics.incremental_tweets")
 
-tweets = sqlCtx.sql("select id, text, entities.user_mentions.name from twitteranalytics.incremental_tweets")
+#tweets = sqlCtx.sql("select id, text, entities.user_mentions.name from twitteranalytics.incremental_tweets")
 
+tweets = sqlCtx.sql("select id, text, entities.user_mentions.name from incremental_tweets")
 
 def sentiment(text):
  words = pattern_split.split(text.lower())
@@ -79,6 +102,7 @@ scoreRenameDF = scoreDF.withColumnRenamed("_1","Candidate").withColumnRenamed("_
 
 sqlCtx.registerDataFrameAsTable(scoreRenameDF, "SCORE_TEMP")
 
-sqlCtx.sql("INSERT OVERWRITE TABLE twitteranalytics.candidate_score \
-	    SELECT Candidate, Score FROM SCORE_TEMP")
+#sqlCtx.sql("INSERT OVERWRITE TABLE twitteranalytics.candidate_score \
+#	    SELECT Candidate, Score FROM SCORE_TEMP")
 
+sqlCtx.sql("INSERT OVERWRITE TABLE candidate_score SELECT Candidate, Score FROM SCORE_TEMP")
